88bfaae2f4d9279864c42525567e2c8fbc4f2b65
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-04 22:41:49 +0800

    - Add content to ContentView

---
+++ Shared/Views/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 4/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Sweater Shop"))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
