7b7f699867f4dde22407cd39eeceba1d0b406019
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-04 22:34:46 +0800

    - Add products data

--- Shared/Models/Product.swift
+++ Shared/Models/Product.swift
@@ -13,3 +13,14 @@ struct Product: Identifiable {
     var image: String
     var price: Int
 }
+
+var productList = [Product(name: "Sweater 1", image:"sweater1", price: 54),
+                   Product(name: "Sweater 2", image:"sweater2", price: 89),
+                   Product(name: "Sweater 3", image:"sweater3", price: 79),
+                   Product(name: "Sweater 4", image:"sweater4", price: 94),
+                   Product(name: "Sweater 5", image:"sweater5", price: 99),
+                   Product(name: "Sweater 6", image:"sweater6", price: 65),
+                   Product(name: "Sweater 7", image:"sweater7", price: 54),
+                   Product(name: "Sweater 8", image:"sweater8", price: 83)]
+
+
