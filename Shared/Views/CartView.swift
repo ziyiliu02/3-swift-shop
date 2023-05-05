637f90f9c3d4b5f10b7cecf2476d8e476c2c5454
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-05 12:53:22 +0800

    - Create CartView and link ContentView to it.

---
+++ Shared/Views/CartView.swift
@@ -0,0 +1,24 @@
+//
+//  CartView.swift
+//  3-swift-shop
+//
+//  Created by Liu Ziyi on 5/5/23.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        ScrollView {
+            Text("Your cart is empty")
+        }
+        .navigationTitle(Text("Your Cart"))
+        .padding(.top)
+    }
+}
+
+struct CartView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartView()
+    }
+}
